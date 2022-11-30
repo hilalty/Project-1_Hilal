d09dd8f4c400bdf759b2d2cf8245f3446f92013a
Hilal Yilmaz <hilaltiftik@gmail.com>
2022-11-30 00:51:12 -0800

    layout

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 console.log("linked");
+
+const container = document.getElementById("container");
+
+const squares = document.getElementsByClassName("square");
+
+function assignColor() {
+    let selectedColor = getRandomColor();
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = selectedColor;
+  }
+}
+
+assignColor();
+
+function getRandomColor() {
+    let a = getColorValue();
+    let b = getColorValue();
+    let c = getColorValue();
+    return "rgb(" + a + "," + b + "," + c + ")";
+}
+console.log(getRandomColor());
+
+function getColorValue() {
+    return Math.floor(Math.random()*256);
+}
